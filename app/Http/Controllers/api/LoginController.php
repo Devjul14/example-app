dccd5ef3c1ea2e75eb13ba0ca09f3965e84a431f
julia eka <eka47577@gmail.com>
2022-08-30 12:55:51 +0700

    api passport

---
+++ app/Http/Controllers/api/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response(['message' => 'Invalid Login!']);
+        }
+    }
+}
